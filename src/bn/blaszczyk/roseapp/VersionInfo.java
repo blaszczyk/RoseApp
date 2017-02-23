560156751f5f399c5cbc79f2c7af36acd890ec24
Michael Blaszczyk <michael.i.blaszczyk@gmail.com>
2017-02-23 03:04:46 +0100

    VersionInfo

---
+++ src/bn/blaszczyk/roseapp/VersionInfo.java
@@ -0,0 +1,45 @@
+package bn.blaszczyk.roseapp;
+
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import bn.blaszczyk.rose.Rose;
+import bn.blaszczyk.roseapp.tools.Messages;
+import bn.blaszczyk.roseapp.view.factories.LabelFactory;
+
+public class VersionInfo {
+	
+	public static final String VERSION_ID = "0.69";
+	
+	private static JPanel infoPanel = null;
+	
+	public static JPanel getInfoPanel()
+	{
+		if(infoPanel == null)
+		{
+			infoPanel = new JPanel(null);
+			infoPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+			infoPanel.setSize(300, 170);
+			
+			JLabel lblRoseApp = LabelFactory.createLabel(Messages.get("based on") + " RoseApp v." + VERSION_ID);
+			lblRoseApp.setBounds(10, 10, 280, 30);
+			infoPanel.add(lblRoseApp);
+			
+			JLabel lblRose = LabelFactory.createLabel(Messages.get("using") + " Rose v." + Rose.VERSION_ID);
+			lblRose.setBounds(10, 50, 280, 30);
+			infoPanel.add(lblRose);
+			
+			JLabel lblGitHub = LabelFactory.createLabel("https://github.com/blaszczyk");
+			lblGitHub.setBounds(10, 90, 280, 30);
+			infoPanel.add(lblGitHub);
+			
+			JLabel lblEmail = LabelFactory.createLabel("michael.i.blaszczyk@gmail.com");
+			lblEmail.setBounds(10, 130, 280, 30);
+			infoPanel.add(lblEmail);
+		}
+		return infoPanel;
+	}
+}
